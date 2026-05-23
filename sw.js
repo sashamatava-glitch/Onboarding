6b95d88fe52c94854cf4824261427d3a0100db5d
sashamatava-glitch <sashamatava@gmail.com>
2026-05-23 23:26:00 +0200

    sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,8 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('v1').then((cache) => cache.addAll(['./index.html', './manifest.json']))
+    caches.open('v8').then((cache) => cache.addAll(['./index.html', './manifest.json', './logo.png']))
   );
 });
-
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
