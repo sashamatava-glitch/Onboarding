fcbccae9dca2d3fb1ed5ce37a753d925f8b33dd6
sashamatava-glitch <sashamatava@gmail.com>
2026-05-23 23:57:25 +0200

    sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,23 @@
+// Changed version to v9 to force phone to update
+const CACHE_NAME = 'oh-za-v9'; 
+
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('v8').then((cache) => cache.addAll(['./index.html', './manifest.json', './logo.png']))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(['./index.html', './manifest.json', './logo.png']))
   );
 });
+
 self.addEventListener('fetch', (e) => {
   e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
+
+// This part kills the old version immediately
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.map((key) => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }));
+    })
+  );
 });
