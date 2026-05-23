3bf30399870db75df33b39eed803c03cd825dc9f
sashamatava-glitch <sashamatava@gmail.com>
2026-05-24 00:23:04 +0200

    sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,24 @@
-// Changed version to v9 to force phone to update
-const CACHE_NAME = 'oh-za-v9'; 
+// Change the version to v10 to force the update
+const CACHE_NAME = 'oh-za-v10'; 
 
+// List of files to keep for offline use
+const assets = [
+  './index.html',
+  './manifest.json',
+  './logo.png'
+];
+
+// 1. Install Event - Downloads the new V10 files
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(['./index.html', './manifest.json', './logo.png']))
-  );
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
-});
-
-// This part kills the old version immediately
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(keys.map((key) => {
-        if (key !== CACHE_NAME) return caches.delete(key);
-      }));
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('V10 Caching files');
+      return cache.addAll(assets);
     })
   );
 });
+
+// 2. Activate Event - Deletes the old V9 (or older) cache
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches
