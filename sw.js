e71b28b93c85858a3666338d2a94c955f9efbf17
sashamatava-glitch <sashamatava@gmail.com>
2026-06-04 23:24:53 +0200

    sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'oh-za-v32';
+const CACHE_NAME = 'oh-za-v34';
 const assets = ['./', './index.html', './manifest.json', './logo.png'];
 
 self.addEventListener('install', (e) => {
